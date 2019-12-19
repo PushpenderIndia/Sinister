d99287cab3786b4d9f2f75b82ab19f1a0c010326
Pushpender <56433698+Technowlogy-Pushpender@users.noreply.github.com>
2019-12-19 16:57:10 +0530

    Improvement

--- installer_linux.sh
+++ installer_linux.sh
@@ -1,20 +1,11 @@
-rm /var/lib/dpkg/lock
-rm /var/cache/apt/archives/lock
-rm /var/lib/apt/lists/lock
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install -y wine32 python3-pip pyinstaller
-
+sudo dpkg --add-architecture i386 && apt-get update && apt-get install wine32 python3-pip pyinstaller -y
 #Downloading Required Executables
-wget https://www.python.org/ftp/python/3.7.4/python-3.7.4.exe
-
+wget "https://www.python.org/ftp/python/3.7.4/python-3.7.4.exe" 
 #Installing Executables in Wine
-wine msiexec /i python-3.7.4.exe
+wine /i python-3.7.4.exe
 wine pywin32-227.win32-py3.7.exe
-
 #Installing Dependencies [Python Modules] in Wine
 sudo wine ~/.wine/drive_c/Python37-32/python.exe -m pip install pyinstaller pynput mss==4.0.3 essential_generators==0.9.2 six==1.12.0 python-xlib==0.25 win32gui pywin32
-
 #Installing Dependencies in Linux 
 sudo python3 -m pip install pynput
 sudo python3 -m pip install mss==4.0.3
