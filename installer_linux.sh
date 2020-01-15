5df9609b421803d3df91d4b781ce09d8ce089a6e
Pushpender <56433698+Technowlogy-Pushpender@users.noreply.github.com>
2020-01-15 11:02:43 +0530

    Update installer_linux.sh

--- installer_linux.sh
+++ installer_linux.sh
@@ -7,8 +7,8 @@ wine pywin32-227.win32-py3.7.exe
 #Installing Dependencies [Python Modules] in Wine
 sudo wine ~/.wine/drive_c/Python37-32/python.exe -m pip install pyinstaller pynput mss==4.0.3 essential_generators==0.9.2 six==1.12.0 python-xlib==0.25 win32gui pywin32
 #Installing Dependencies in Linux 
-sudo python3 -m pip install pynput
-sudo python3 -m pip install mss==4.0.3
-sudo python3 -m pip install essential_generators==0.9.2
-sudo python3 -m pip install six==1.12.0 
-sudo python3 -m pip install python-xlib==0.25
+sudo pip3 install pynput
+sudo pip3 install mss==4.0.3
+sudo pip3 install essential_generators==0.9.2
+sudo pip3 install six==1.12.0 
+sudo pip3 install python-xlib==0.25
