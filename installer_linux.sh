860db64334f8c1b8f192bfe8dc930ebf707dcf31
Pushpender <56433698+Technowlogy-Pushpender@users.noreply.github.com>
2020-04-09 21:10:43 +0530

    Update installer_linux.sh

--- installer_linux.sh
+++ installer_linux.sh
@@ -2,7 +2,7 @@ sudo dpkg --add-architecture i386 && apt-get update && apt-get install wine32 py
 #Downloading Required Executables
 wget "https://www.python.org/ftp/python/3.7.4/python-3.7.4.exe" 
 #Installing Executables in Wine
-wine /i python-3.7.4.exe
+wine python-3.7.4.exe
 wine pywin32-227.win32-py3.7.exe
 #Installing Dependencies [Python Modules] in Wine
 sudo wine ~/.wine/drive_c/Python37-32/python.exe -m pip install pyinstaller pynput mss==4.0.3 essential_generators==0.9.2 six==1.12.0 python-xlib==0.25 win32gui pywin32
